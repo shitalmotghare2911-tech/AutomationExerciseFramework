0d0f584cd970ddfcdd98011cebc44cb88b403684
shital <shitalmotghare2911@gmail.com>
2026-08-01 22:56:42 +0530

    invalid test

---
+++ src/main/java/Pages/LoginPage.java
@@ -0,0 +1,50 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage 
+{
+	WebDriver driver;
+	public LoginPage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	//Locators
+	By signup = By.xpath(" //a[@href=\"/login\"]");
+	By Email = By.xpath("//input[@data-qa=\"login-email\"]");
+	By password = By.xpath("//input[@data-qa=\"login-password\"]");
+	
+	By Login = By.xpath("//button[@data-qa=\"login-button\"]");
+	
+	//Methods
+	
+	public void ClickSignUp()
+	{
+		driver.findElement(signup).click();
+	}
+	
+	public void EnterEmail(String userName)
+	{
+		driver.findElement(Email).sendKeys(userName);
+	}
+	
+	public void EnterPassword(String userPass)
+	{
+		driver.findElement(password).sendKeys(userPass);
+	}
+	public void ClickLoginBtn()
+	{
+		driver.findElement(Login).click();
+	}
+
+	public String getErrorMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+	
+
+}
---
+++ src/test/java/TestCases/LoginTest.java
@@ -0,0 +1,40 @@
+package TestCases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Base.BaseClass;
+import Pages.LoginPage;
+
+public class LoginTest extends BaseClass
+{
+	@Test
+	public void verifyValidLogin()
+	{
+		LoginPage login = new LoginPage(driver);
+	    login.ClickSignUp();
+	    login.EnterEmail("shital@gmail.com");
+	    login.EnterPassword("password@123");
+	    login.ClickLoginBtn();
+	}
+	@Test
+	public void verifyInvalidLogin()
+	{
+		LoginPage login = new LoginPage(driver);
+		login.ClickSignUp();
+		login.EnterEmail("1234");
+		login.EnterPassword("....");
+		login.ClickLoginBtn();
+		
+		 String actualMessage = login.getErrorMessage();
+
+	     String expectedMessage = "Your email or password is incorrect!";
+
+	        Assert.assertEquals(actualMessage, expectedMessage);
+
+	}
+	
+
+}
