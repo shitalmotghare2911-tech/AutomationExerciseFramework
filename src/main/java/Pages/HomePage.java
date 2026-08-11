973f3e67d99907be93bccd24f98c3fb8f5e28c7b
shital <shitalmotghare2911@gmail.com>
2026-08-11 14:31:19 +0530

    Added homepage automation

---
+++ src/main/java/Pages/HomePage.java
@@ -0,0 +1,29 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage 
+{
+	WebDriver driver;
+	public HomePage(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	private By productsLink = By.xpath("//a[@href=\"/products\"]");
+	private By cartLink = By.xpath("//a[@href=\"/view_cart\"]");
+	private By logoutLink = By.xpath("//a[@href=\"/logout\"]");
+	private By deleteAccountLink = By.xpath("//a[@href=\"/delete_account\"]");
+	
+	private By testcasesLink = By.xpath("//a[@href=\"/test_cases\"]");
+	private By cartLink = By.xpath("//a[@href=\"/view_cart\"]");
+	private By cartLink = By.xpath("//a[@href=\"/view_cart\"]");
+	private By cartLink = By.xpath("//a[@href=\"/view_cart\"]");
+	private By cartLink = By.xpath("//a[@href=\"/view_cart\"]");
+
+	
+	
+	
+
+}
